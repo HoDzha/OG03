9368a4c044bc5735f0c7314074296f86e71fd125
HoDzha <hodzha@gmail.com>
2024-08-11 22:37:17 +0500

    add new const

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import pygame
 
 pygame.init()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Игра тир")
+icon = pygame.image.load("C:\Users\user\Desktop\Обучение Pyhon+ChtGpt\OG03\5659_800.jpg")
+
 # pygame.display.set_caption("Test")
 # screen = pygame.display.set_mode((800, 600))
 running = True
